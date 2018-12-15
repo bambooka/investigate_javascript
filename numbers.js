ad6af4b49fc442eb42bb008042f7ce40cb81f256
OlaBooka <booka@yandex.ru>
2018-12-15 16:14:01 +0300

    find a sum with accuracy

--- numbers.js
+++ numbers.js
@@ -4,3 +4,6 @@ let firstNumber = +prompt('ведите первое число', 0);
 let secondNumber = +prompt('введите второе число', 0);
 
 alert(firstNumber + secondNumber);
+
+// a sum with accuracy
+alert((0.3 * 10 + 0.2 * 10) / 10);
