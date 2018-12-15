6d334ffa3cbf450595e086c8e1bb02402e95aa4c
OlaBooka <booka@yandex.ru>
2018-12-15 15:48:50 +0300

    fine a sum of input numbers

---
+++ numbers.js
@@ -0,0 +1,7 @@
+// find a sum of input numbers
+
+let firstNumber = prompt('ведите первое число', 0);
+let secondNumber = prompt('введите второе число', 0);
+
+let result = parseInt(firstNumber) + parseInt(secondNumber);
+alert(result + ' сумма ваших чисел');
